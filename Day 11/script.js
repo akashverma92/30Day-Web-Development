cd9d13166270b9a1c65abcd8513267b8304da0ba
Akash Kumar Verma <138753810+akashverma92@users.noreply.github.com>
2024-10-31 22:51:22 +0530

    Add files via upload

---
+++ Day 11/script.js
@@ -0,0 +1,13 @@
+
+let name = "John Doe";
+const age = 25;
+var isStudent = true;
+
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+
+name = "Jane Doe";
+console.log("Updated Name:", name);
