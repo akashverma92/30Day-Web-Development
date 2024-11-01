ad4d17407eeb489d993c1dcb8fc227753532a156
Akash Kumar Verma <138753810+akashverma92@users.noreply.github.com>
2024-11-01 22:59:27 +0530

    Add files via upload

---
+++ Day 12/scripit.js
@@ -0,0 +1,16 @@
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+greet("John");
+let score = 85;
+if (score > 90) {
+    console.log("Excellent!");
+} else if (score > 70) {
+    console.log("Good Job!");
+} else {
+    console.log("Keep Trying!");
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration:", i);
+}
