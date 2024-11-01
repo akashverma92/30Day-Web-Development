8a8d14e389cbf19f10fad2f87e619f71ea88b6de
Akash Kumar Verma <138753810+akashverma92@users.noreply.github.com>
2024-11-01 23:00:45 +0530

    Add files via upload

---
+++ Day 13/scripit.js
@@ -0,0 +1,9 @@
+let fruits = ["Apple", "Banana", "Orange"];
+console.log("First Fruit:", fruits[0]); 
+fruits[1] = "Mango";
+console.log("Updated Fruits:", fruits);
+fruits.push("Grapes");
+console.log("Fruits after adding:", fruits); 
+let lastFruit = fruits.pop();
+console.log("Last Fruit Removed:", lastFruit); 
+console.log("Fruits:", fruits); 
