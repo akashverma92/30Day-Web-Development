125b9c291852bd46ea1310cd8ecbb7a19d86fa8c
Akash Kumar Verma <138753810+akashverma92@users.noreply.github.com>
2024-11-04 00:19:39 +0530

    Add files via upload

---
+++ Day 17/s.js
@@ -0,0 +1,8 @@
+let people = [
+    { name: "John", age: 30 },
+    { name: "Jane", age: 25 },
+    { name: "Jack", age: 35 }
+];
+people.forEach(function(person) {
+    console.log(person.name + " is " + person.age + " years old.");
+});
